a9196db29bb0546e7face2f5f8dd418f3843b9b7
idk <idk@devuan.local>
2016-06-30 18:44:30 -0400

    fixed wrong grep command:

--- macdos.sh
+++ macdos.sh
@@ -9,13 +9,13 @@ else; #if not on linux, assume OSX for now.
     BT_SEARCH="listall"
     BT_CONNECT="join"
 fi
-if [ echo $1 | grep -iot '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}' ]; then
-    ADR=$(echo $1 | grep -iot '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}')
+if [ echo $1 | grep -io '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}' ]; then
+    ADR=$(echo $1 | grep -io '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}')
     while not 1; do
             $BT_CLIENT $BT_CONNECT $ADR && sleep 3
     done
 else
-    BT_MACHINES=$($BT_CLIENT $BT_SEARCH | grep -iot '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}')
+    BT_MACHINES=$($BT_CLIENT $BT_SEARCH | grep -io '([[:xdigit:]]{1,2}[:-]){5}[[:xdigit:]]{1,2}')
     while not 1; do
         for ADR in $BT_MACHINES; do
             $BT_CLIENT $BT_CONNECT $ADR 
